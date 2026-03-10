f766bf2806228fb58be082253505107bd38a306c
cmdrumright <cdrumright@protonmail.com>
2026-03-10 15:30:51 -0500

    changed ship_view to list_ships to build query accountint for expand

--- views/ship_view.py
+++ views/ship_view.py
@@ -53,22 +53,38 @@ def delete_ship(pk):
     return True if number_of_rows_deleted > 0 else False
 
 
-def list_ships():
+def list_ships(url):
+    # Initialize query
+    select_string = """
+        SELECT
+            s.id,
+            s.name,
+            s.hauler_id"""
+    from_string = """
+        FROM Ship s
+        """
+    # Check if "_expand" is used
+    if "_expand" in url["query_params"]:
+        for value in url["query_params"]["_expand"]:
+            if value == "hauler":
+                select_string += """,
+                    h.id haulerId,
+                    h.name haulerName,
+                    h.dock_id"""
+                from_string += """
+                    JOIN Hauler h
+                        ON h.id = s.hauler_id
+                        """
+
+    query_string = select_string + from_string
+
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute(
-            """
-        SELECT
-            s.id,
-            s.name,
-            s.hauler_id
-        FROM Ship s
-        """
-        )
+        db_cursor.execute(query_string)
         query_results = db_cursor.fetchall()
 
         # Initialize an empty list and then add each dictionary to it
